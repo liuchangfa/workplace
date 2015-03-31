163c979ba9455b86ae7e1d407ca2960c323a2d57
liuchangfa <290586299@qq.com>
2015-04-01 00:47:49 +0800

    sss

--- AndroidProject/DataSaveTechnology/MyDataSQL/gen/com/yunnex/mydatasql/R.java
+++ AndroidProject/DataSaveTechnology/MyDataSQL/gen/com/yunnex/mydatasql/R.java
@@ -25,9 +25,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080006;
         public static final int add_data=0x7f080001;
         public static final int create_database=0x7f080000;
+        public static final int delete_data=0x7f080003;
+        public static final int query_data=0x7f080004;
+        public static final int replace_data=0x7f080005;
+        public static final int update_data=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,10 +40,14 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050003;
+        public static final int action_settings=0x7f050007;
         public static final int add=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int create=0x7f050001;
+        public static final int delete=0x7f050004;
+        public static final int query=0x7f050005;
+        public static final int replace=0x7f050006;
+        public static final int update=0x7f050003;
     }
     public static final class style {
         /** 
