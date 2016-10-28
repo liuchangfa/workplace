474ad60788a7d0b49e2dbb9d88f69ce07e858239
liuchangfa <290586299@qq.com>
2016-10-28 19:48:46 +0800

    sfcsdfsdf

--- AndroidProject/Summary/Summary/Summary/src/main/java/com/herman/summary/MainActivity.java
+++ AndroidProject/Summary/Summary/Summary/src/main/java/com/herman/summary/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 //		NVRAM.formatSecretKeys();
-		NVRAM.deleteSecretKeys(2);
+//		NVRAM.deleteSecretKeys(2);
 		byte[] bytes = NVRAM.KEY_PAY1.getBytes();
 		Log.e(TAG, "KEY_PAY1 = " + NVRAM.KEY_PAY1);
 		Log.e(TAG, "key1_hex = " + FormatConversion.byteTohex(bytes));
-//		boolean flag = NVRAM.saveSecretKeys(22,NVRAM.KEY_PAY1);
-//		Log.e(TAG, "是否写入成功" + flag);
-		String keyStr= NVRAM.readSecretKeys(25, null);
+		boolean flag = NVRAM.saveSecretKeys(22,NVRAM.KEY_PAY1);
+		Log.e(TAG, "是否写入成功" + flag);
+		String keyStr= NVRAM.readSecretKeys(22, null);
 		Log.e(TAG, "读取秘钥-->:" + keyStr);
 	}
 }
